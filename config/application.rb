fb0671d1be450ae840c6c1b17f436ea9deb7c7c2
Piyush Chauhan <piyushchauhan2011@gmail.com>
2015-09-29 20:31:50 +1000

    Changed configuration for mongoid.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Boombox
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+        g.orm :mongoid
+    end
   end
 end
